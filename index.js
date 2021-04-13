83b0d5653e9c463713be0fa79df74d2f3930e53a
Jan Bölsche <regular.gonzales@googlemail.com>
2021-04-13 08:50:39 +0200

    Feature: set auto:false to tkae control over when accept() is called and be able to receive any errors

--- index.js
+++ index.js
@@ -3,51 +3,77 @@ const Log = require('./log')
 
 exports.name = 'autoinvite'
 exports.version = require('./package.json').version
-exports.manifest = {}
+exports.manifest = {
+  useInviteCode: 'async'
+}
 
 exports.init = function (ssb, config) {
   const {error, warning, notice, info} = Log(ssb, exports.name)
 
   let code = config && config.autoinvite
-  if (!code) {
-    notice('No autoinvite section in config')
-    return {}
+  let auto = true
+  if (code !== undefined && typeof code !== 'string') {
+    auto = code.auto !== false
+    code = code.code
   }
-  info('found invite code')
-  ssb.whoami( (err, feed) => {
-    if (err) {
-      error('whoami failed')
-      return {}
+  if (code && !auto) {
+    notice('will not auto-invite because autoinvite.auto is set to false')
+  }
+
+  if (auto) {
+    useInviteCode(err=>{})
+  }
+
+  return {
+    useInviteCode
+  }
+  
+  function useInviteCode(cb) {
+    if (!code) {
+      const msg = 'No autoinvite section in config'
+      notice(msg)
+      return cb(new Error(msg))
     }
-    let doAccept = true
-    pull(
-      ssb.createUserStream({
-        id: feed.id,
-        values: true,
-        keys: false
-      }),
-      pull.asyncMap( (value, cb) => {
-        const content = value.content
-        if (content && content.type == 'pub') {
-          doAccept = false
-          return cb(true)
-        }
-        cb(null, value)
-      }),
-      pull.onEnd( err => {
-        if (err) throw err
-        if (!doAccept) {
-          info('Already knows about a pub')
-          return
-        }
-        notice('calling invite.accept')
-        ssb.invite.accept(code, (err, results) => {
-          if (err) return error('invite.accept failed:' + err.message)
-          notice('invite accepted %o', results)
+    info('found invite code')
+    ssb.whoami( (err, feed) => {
+      if (err) {
+        const msg = 'whoami failed'
+        error(msg)
+        return cb(new Error(msg))
+      }
+      let doAccept = true
+      pull(
+        ssb.createUserStream({
+          id: feed.id,
+          values: true,
+          keys: false,
+        }),
+        pull.asyncMap( (value, cb) => {
+          const content = value.content
+          if (content && content.type == 'pub') {
+            doAccept = false
+            return cb(true)
+          }
+          cb(null, value)
+        }),
+        pull.onEnd( err => {
+          if (err) return cb(err)
+          if (!doAccept) {
+            info('Already knows about a pub')
+            return cb(null)
+          }
+          notice('calling invite.accept')
+          ssb.invite.accept(code, (err, results) => {
+            if (err) {
+              error('invite.accept failed:' + err.message)
+              return cb(err)
+            }
+            notice('invite accepted %o', results)
+            cb(null)
+          })
         })
-      })
-    )
-  })
-  return {}
+      )
+    })
+  }
 }
 
--- test.js
+++ test.js
@@ -1,75 +1,71 @@
 const test = require('tape')
+const crypto = require('crypto')
 const pull = require('pull-stream')
 const ssbKeys = require('ssb-keys')
-const {createSbot} = require('scuttlebot')
+const CreateSbot = require('tre-bot')
 const rimraf = require('rimraf')
 
 const lucyKeys = ssbKeys.generate()
 
-test('do nothing if no autofoinvite is specified', t => {
-  const myTestSbot = createSbot().use(require('.'))({
-    temp: true,
-    keys: lucyKeys
-  })
+function testbot(config, cb) {
+  CreateSbot().use(require('.'))(Object.assign({
+    caps: {
+      shs: crypto.randomBytes(32)
+    },
+    temp: !config.path 
+  }, config), lucyKeys, cb)
+}
 
-  setTimeout(() => {
-    pull(
-      myTestSbot.createLogStream({}),
-      pull.collect((err, msgs) => {
-        t.error(err)
-        t.equals(msgs.length, 0, 'no message')
-        myTestSbot.close( () => t.end() )
-      })
-    )
-  }, 300)
+test('do nothing if no autofoinvite is specified', t => {
+  testbot({}, (err, myTestSbot) =>{
+    t.error(err)
+    setTimeout(() => {
+      pull(
+        myTestSbot.createLogStream({}),
+        pull.collect((err, msgs) => {
+          t.error(err)
+          t.equals(msgs.length, 0, 'no message')
+          myTestSbot.close( () => t.end() )
+        })
+      )
+    }, 300)
+  })
 })
 
 test('Call invite.accept if a code was specified', t => {
-  const myTestSbot = createSbot().use(require('.'))({
-    temp: true,
-    keys: lucyKeys,
+  testbot({
     autoinvite: 'code'
-  })
-  myTestSbot.invite = {
-    accept: (code, cb) => {
-      t.equal(code, 'code')
-      cb(null, {blah: true})
-      t.pass()
-      myTestSbot.close( ()=> t.end() )
+  }, (err, myTestSbot) =>{
+    t.error(err)
+    myTestSbot.invite = {
+      accept: (code, cb) => {
+        t.equal(code, 'code')
+        cb(null, {blah: true})
+        t.pass()
+        myTestSbot.close( ()=> t.end() )
+      }
     }
-  }
+  })
 })
 
-test('dont call invite.accept if the user already knows about a pub', t => {
-  const path = '/tmp/test_autoinvite_preexisting'
-  rimraf.sync(path)
-
-  function publish(cb) {
-    const myTestSbot = createSbot()({
-      path,
-      keys: lucyKeys
-    })
-    const lucy = myTestSbot.createFeed(lucyKeys)
-
-    lucy.publish({
-      type: 'pub'
-    }, (err, msg) => {
-      if (err) throw err
-      myTestSbot.close(cb)
-    }) 
-  }
-      
-  publish( err => {
+test("don't call invite.accept on startup if auto==false", t => {
+  let startup = true
+  testbot({
+    autoinvite: {
+      code: 'code',
+      auto: false
+    }
+  }, (err, myTestSbot) => {
     t.error(err)
-    
-    const myTestSbot = createSbot().use(require('.'))({
-      path,
-      keys: lucyKeys,
-      autoinvite: 'code'
-    })
     myTestSbot.invite = {
-      accept: ()=> {
-        t.fail()
+      accept: (code, cb) => {
+        if (startup) {
+          t.fail('accept() was called during startup when it shouldnt')
+        } else {
+          t.pass('accept() was called')
+          t.equal(code, 'code')
+          cb(null)
+        }
       }
     }
 
@@ -78,11 +74,61 @@ test('dont call invite.accept if the user already knows about a pub', t => {
         myTestSbot.createLogStream({}),
         pull.collect((err, msgs) => {
           t.error(err);
-          t.equals(msgs.length, 1, 'one message');
-          myTestSbot.close( () => t.end() )
+          t.equals(msgs.length, 0, 'no message');
+          startup = false
+          myTestSbot.autoinvite.useInviteCode( err=>{
+            t.error(err)
+            myTestSbot.close( () => t.end() )
+          })
         })
       )
-    }, 300)
+    })
+  })
+})
+
+test("don't call invite.accept if the user already knows about a pub", t => {
+  const path = '/tmp/test_autoinvite_preexisting'
+  rimraf.sync(path)
+
+  function publish(cb) {
+    testbot({path, host: 'localhost'}, (err, myTestSbot) =>{
+      const lucy = myTestSbot.createFeed(lucyKeys)
+
+      lucy.publish({
+        type: 'pub'
+      }, (err, msg) => {
+        if (err) throw err
+        myTestSbot.close(cb)
+      }) 
+    })
+  }
+      
+  publish( err => {
+    if (err) throw err
+
+    testbot({
+      path,
+      autoinvite: 'code'
+    }, (err, myTestSbot) => {
+      t.error(err)
+      myTestSbot.invite = {
+        accept: ()=> {
+          t.fail('accept() was called when it shouldnt')
+        }
+      }
+
+      setTimeout( ()=>{
+        pull(
+          myTestSbot.createLogStream({}),
+          pull.collect((err, msgs) => {
+            t.error(err);
+            t.equals(msgs.length, 1, 'one message');
+            myTestSbot.close( () => t.end() )
+          })
+        )
+      }, 300)
+    })
+
   })
 })
 
